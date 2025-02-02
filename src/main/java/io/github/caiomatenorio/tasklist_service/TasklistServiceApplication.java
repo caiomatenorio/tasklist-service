f89f01e23b1cc69272bbae205e67cfdad6a9f35e
Caio Tenório <caiomatenorio@gmail.com>
2025-02-02 07:47:08 -0300

    initialize tasklist-service project with Gradle setup and basic configuration

---
+++ src/main/java/io/github/caiomatenorio/tasklist_service/TasklistServiceApplication.java
@@ -0,0 +1,17 @@
+package io.github.caiomatenorio.tasklist_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import io.github.caiomatenorio.tasklist_service.config.AppProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class TasklistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/caiomatenorio/tasklist_service/TasklistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caiomatenorio.tasklist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
